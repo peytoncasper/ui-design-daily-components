f21b150e0cff5679043103e7feeaeaf007f2b0df
Peyton Casper <peytoncas@gmail.com>
2024-01-09 23:43:50 -0800

    adding first component

---
+++ src/components/profile-page-header-card-day-1580.jsx
@@ -0,0 +1,12 @@
+
+
+export default function ProfilePageHeaderCardDay1580() {
+    return (
+        <>
+            <div style={{width:"1051px", height:"418px", background: "#FFFAF2", flexDirection:"row", display:"flex"}}>
+                <div style={{display:"flex", flexGrow: "1"}}></div>
+                <div style={{display:"flex", flexGrow: "1", padding: "40px 40px 40px 40px"}}>test</div>
+            </div>
+        </>
+    )
+}
