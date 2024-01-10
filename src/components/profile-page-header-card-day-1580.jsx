1c76aa458df2c3c4177d19a2efcd616e7b6d7ef9
Peyton Casper <peytoncas@gmail.com>
2024-01-10 00:53:26 -0800

    Adding profile picture to profile header component

--- src/components/profile-page-header-card-day-1580.jsx
+++ src/components/profile-page-header-card-day-1580.jsx
@@ -4,7 +4,11 @@ export default function ProfilePageHeaderCardDay1580() {
     return (
         <>
             <div style={{width:"1051px", height:"418px", background: "#FFFAF2", flexDirection:"row", display:"flex"}}>
-                <div style={{display:"flex", flexGrow: "1"}}></div>
+                <div style={{display:"flex", flexGrow: "1", padding: "40px 40px 40px 40px"}}>
+                    <div style={{width: "100px", height: "100px", borderRadius: "39px", backgroundPosition: "100px 200px", background: "url(profilePicture.png), lightgray -12.783px -24.485px / 194% 242.5% no-repeat"}}>
+
+                    </div>
+                </div>
                 <div style={{display:"flex", flexGrow: "1", padding: "40px 40px 40px 40px"}}>test</div>
             </div>
         </>
